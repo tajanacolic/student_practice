085d6d1abdc3c21e8f3835f986351340b576eff2
Tajana Čolić <74814005+tajanacolic@users.noreply.github.com>
2022-01-19 12:34:19 +0100

    Some database and model code
    Not finished yet with model Practice and Database.

--- Database.php
+++ Database.php
@@ -2,7 +2,80 @@
 
 namespace app;
 
+use app\models\Practice;
+use PDO;
+
 class Database
 {
+
+    public $pdo = null;
+    public static ?Database $db = null;
+
+    public function  __construct() {
+
+        $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=practice_crud', 'root', '');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        self::$db = $this;
+    }
+
+    public function getPractice()
+    {
+        $statement = $this->pdo->prepare('SELECT * FROM practice ORDER BY create_date ASC');
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getPracticeById($practice_id) {
+
+        $statement = $this->pdo->prepare('SELECT * FROM practice WHERE practice_id = :practice_id');
+        $statement->bindValue(':practice_id', $practice_id);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+
+    }
+
+    public function updatePractice(Practice $practice)
+    {
+        $statement = $this->pdo->prepare("UPDATE practice SET practice_activity = :practice_activity
+                                        WHERE practice_id = :practice_id");
+        $statement->bindValue(':practice_activity', $practice->practice_activity);
+        $statement->bindValue(':practice_id', $practice->practice_id);
+
+        $statement->execute();
+    }
+
+    public function createPractice(Practice $practice)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO practice (practice_name, practice_email, practice_phone, 
+                      practice_education, practice_type, practice_activity, create_date)
+                VALUES (:practice_name, :practice_email, :practice_phone, :practice_education, :practice_type, :practice_activity, :date)");
+        $statement->bindValue(':practice_name', $practice->practice_name);
+        $statement->bindValue(':practice_email', $practice->practice_email);
+        $statement->bindValue(':practice_phone', $practice->practice_phone);
+        $statement->bindValue(':practice_education', $practice->practice_type);
+        $statement->bindValue(':practice_type', $practice->practice_type);
+        $statement->bindValue(':practice_activity', $practice->practice_activity);
+        $statement->bindValue(':date', date('Y-m-d H:i:s'));
+
+        $statement->execute();
+    }
+
+    public function deletePractice($practice_id) {
+
+        $statement = $this->pdo->prepare('DELETE FROM practice WHERE practice_id = :practice_id');
+        $statement->bindValue(':practice_id', $practice_id);
+        $statement->execute();
+
+
+    }
+
+    public function getAdmin(){
+        $statement = $this -> pdo -> prepare('SELECT * FROM admin');
+        $statement -> execute();
+        return $statement -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
 
 }
--- models/Practice.php
+++ models/Practice.php
@@ -2,7 +2,82 @@
 
 namespace app\models;
 
+use app\Database;
+
 class Practice
 {
+
+    public ?int $practice_id = null;
+    public ?string $practice_name = null;
+    public ?string $practice_email = null;
+    public ?string $practice_phone = null;
+    public ?string $practice_education = null;
+    public ?string $practice_type = null;
+    public ?boolean $practice_activity = null;
+
+    public function load($data) {
+
+        $this->practice_id = $data['practice_id'] ?? null;
+        $this->practice_name = $data['practice_name'];
+        $this->practice_email = $data['practice_email'];
+        $this->practice_phone = $data['practice_phone'];
+        $this->practice_education = $data['practice_education'];
+        $this->practice_type = $data['practice_type'];
+        $this->practice_activity = $data['practice_activity'];
+
+    }
+
+    public function save() {
+
+
+
+        $errors = [];
+
+        if (!$this->practice_name) {
+
+            $errors[] = 'Your name is required.';
+
+        }
+
+        if (!$this->practice_email) {
+
+            $errors[] = 'Your email is required.';
+
+        }
+
+        if (!$this->practice_phone) {
+
+            $errors[] = 'Your phone number is required.';
+
+        }
+
+        if (!$this->practice_education) {
+
+            $errors[] = 'Your level of education is required.';
+
+        }
+        if (!$this->practice_type) {
+
+            $errors[] = 'Your practice type is required.';
+
+        }
+
+        if (empty($errors)) {
+
+            $db = Database::$db;
+
+            if ($this->practice_id) {
+
+                $db->updatePractice($this);
+
+            } else {
+
+                $db->createPractice($this);
+
+            }
+
+        }
+        return $errors;
+    }
 
 }
