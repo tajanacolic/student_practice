fb02c1ab9cbbec686737b4599a059e23c2b9089e
Tajana Čolić <74814005+tajanacolic@users.noreply.github.com>
2022-01-19 10:43:39 +0100

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'cor4/student_practice',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cor4/student_practice' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
