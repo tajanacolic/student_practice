55bda5b26eb577ecd53ce48c9e1c6adb5f419a5d
Mirza Mahmutovic <97639413+MMahmutovicc@users.noreply.github.com>
2022-01-19 14:18:24 +0100

    :)

--- controllers/PracticeController.php
+++ controllers/PracticeController.php
@@ -2,7 +2,57 @@
 
 namespace app\controllers;
 
+use app\models\Practice;
+use app\Router;
+
 class PracticeController
 {
+    public static function index(Router $router) 
+    {
+        if($_SESSION['name'] === 'admin')
+        {
+            $practices = $router->db->getPractice();
+            $router -> renderView('practice/practicesview',[
+                'practices' => $practices
+            ]);
+        }
+        else
+        {
+            $errors = [];
+            $practiceData = [
+                'practice_name' => '',
+                'practice_surname' => '',
+                'practice_email' => '',
+                'practice_phone' => '',
+                'practice_education' => '',
+                'practice_type' => '',
+                'practice_activity' => true,
+            ];
+            if($_SERVER['REQUEST_METHOD'] === 'POST')
+            {
+                $practiceData['practice_name'] = $_POST['practice_name'];
+                $practiceData['practice_surname'] = $_POST['practice_surname'];
+                $practiceData['practice_email'] = $_POST['practice_email'];
+                $practiceData['practice_phone'] = $_POST['practice_phone'];
+                $practiceData['practice_education'] = $_POST['practice_education'];
+                $practiceData['practice_type'] = $_POST['practice_type'];
+                $practiceData['practice_activity'] = $_POST['practice_activity'];
+                $practice = new Practice();
+                $practice->load($practiceData);
+                $errors = $practice->save();
 
+                if(empty($errors))
+                {
+                    $_SESSION['activity'] = 'applied';
+                    $_SESSION['applied'] = true;
+                    header('Location:/practice/view');
+                    exit;
+                }
+                $router->renderView('practice',[
+                    'practice' => $practiceData,
+                    'errors' => $errors
+                ]);
+            }
+        }
+    }
 }
--- public/index.php
+++ public/index.php
@@ -11,6 +11,16 @@ use app\controllers\PracticeController;
 
 $router = new Router();
 
+$router->get('/', [PracticeController::class, 'index']);
+$router->get('/practice', [PracticeController::class, 'index']);
+$router->get('/practice/', [PracticeController::class, 'index']);
+$router->get('/practice/index', [PracticeController::class, 'index']);
+$router->post('/', [PracticeController::class, 'index']);
+$router->post('/practice', [PracticeController::class, 'index']);
+$router->post('/practice/', [PracticeController::class, 'index']);
+$router->post('/practice/index', [PracticeController::class, 'index']);
+$router->get('/practice/view',[PracticeController::class, 'view']);
+
 
 $router->get('/practice/signin',[AdminController::class,'signin']);
 $router->post('/practice/signin',[AdminController::class,'signin']);
